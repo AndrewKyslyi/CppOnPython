817901d3ee969c190da4c0eb596c0d94a9a48de2
AndrewKyslyi <skorpypro@gmail.com>
2024-07-18 23:46:44 +0300

    omg

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 import variables
 from threading import Thread
 
-thread1 = Thread(target=hello())
-thread2 = Thread(target=world())
+#thread1 = Thread(target=hello())
+#thread2 = Thread(target=world())
 
 while True:
     print('Hello! This is "World Of Magic"\nIn this universe u can choose any way u like! Maybe its just like "open-world" console game xD\nHope ull have fun!')

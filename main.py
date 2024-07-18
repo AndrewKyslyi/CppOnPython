0be6a722b1db0cd931deea922941d2162574c446
AndrewKyslyi <skorpypro@gmail.com>
2024-07-18 23:18:21 +0300

    it was so hard

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-import _random as _r
+import random as r
+import time
+import variables
+from threading import Thread
 
 
-class Player():
-    def __init__(self) -> None:
-        self.hp = 100
-        self.mana = 10
-        self.resistance = 0
-        self.mag_resistance = 0
-        self.mag_damage = 0
-        self.damage = 10
-        super().__init__()
+while True:
+    print('Hello! This is "World Of Magic"\nIn this universe u can chose any way u like! Maybe its just like "open-world" console game xD\nHope ull have fun!')
+    time.sleep(3)
+    print("So...")
+    time.sleep(1)
+    print("Lets start!")
+    break
+    print()
 
 
-class Weapon():
-    def __init__(self) -> None:
-        self.rarity = ""
-        self.name = ""
-        self.add_hp = 0
-        self.add_damage = 0
-        self.add_resistance = 0
-        self.add_mag_damage = 0
-        self.add_mag_resistance = 0
-        self.add_mana = 0
-        super().__init__()
 
-    
-class Sword(Weapon):
-    def __init__(self) -> None:
-        super().__init__()
-        
-
-BasicSword = Sword()
-BasicSword.name = "Basic Sword"
-BasicSword.rarity = "Common"
-print(BasicSword.name)
---
+++ variables.py
@@ -0,0 +1,100 @@
+import random as r
+import time
+
+
+class Player():
+    def __init__(self) -> None:
+        self.hp = 100
+        self.mana = 10
+        self.resistance = 0
+        self.mag_resistance = 0
+        self.mag_damage = 0
+        self.damage = 10
+        self.attack_speed = 1
+        self.gold = 0
+        super().__init__()
+
+
+class Object():
+    def __init__(self) -> None:
+        self.rarity = ""
+        self.name = ""
+        self.add_hp = 0
+        self.add_damage = 0
+        self.add_resistance = 0
+        self.add_mag_damage = 0
+        self.add_mag_resistance = 0
+        self.add_mana = 0
+        self.add_attack_speed = 0
+        super().__init__()
+
+    def random_rarity(self):
+        rarity = None
+        random = r.randint(0, 100)
+
+        if random >= 0 and random <= 50:
+            rarity = "Common"
+        elif random > 50 and random <= 80:
+            rarity = "Uncommon"
+        elif random > 80 and random <= 90:
+            rarity = "Rare"
+        elif random > 90 and random <= 97:
+            rarity = "Mithic"
+        else:
+            rarity = "Legendary"
+
+
+class Sword(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Axe(Object):
+    def __init__(self) -> None:
+        super().__init__()
+    
+
+class Bow(Object):
+    def __init__(self) -> None:
+        super().__init__()    
+
+
+class Wand(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Helmet(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Chestplate(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Leggings(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Boots(Object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Gold():
+    def __init__(self) -> None:
+        super().__init__()
+
+
+def random_rarity(obj):
+    rarity = None
+    
+    return rarity
+
+
+BasicSword = Sword()
+BasicSword.name = "Basic Sword"
+BasicSword.rarity = "Common"
